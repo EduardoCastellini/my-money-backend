ed14b5e1d74788374669b26987cd5160699a6aa3
Eduardo Castellini <eduardo.castellini@kanastra.com.br>
2024-05-15 14:04:39 -0300

    feat: debt consumer

--- src/modules/workers/consumers/debt-consumer.ts
+++ src/modules/workers/consumers/debt-consumer.ts
@@ -1,11 +1,19 @@
 import { Processor, Process } from '@nestjs/bull';
 import { Job } from 'bull';
+import { IDebtRepository } from 'src/modules/core/contracts/debt-repository.interface';
+import { DebtEntity } from 'src/modules/core/domain/entities/debt.entity';
 
 @Processor('debt')
 export class DebtConsumer {
+  constructor(private readonly debtRepository: IDebtRepository) {}
+
   @Process('process')
-  async process(job: Job<unknown>) {
-    console.log(job.data);
+  async process(job: Job<DebtEntity>) {
+    console.log(job.data.id);
+
+    const result = await this.debtRepository.findOne(job.data.id);
+
+    console.log('result: ', result.status);
 
     return {
       message: 'Debt processed',
--- src/modules/workers/workers.module.ts
+++ src/modules/workers/workers.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { InfraModule } from '../infra/infra.module';
 import { CoreModule } from '../core/core.module';
 import { DebtConsumer } from './consumers/debt-consumer';
+import { RepositoryProviders } from 'src/providers/repository-provider.enum';
+import { DebtRepository } from '../infra/db/repositories/debt.repository';
 
 @Module({
   imports: [InfraModule, CoreModule],
-  providers: [DebtConsumer],
+  providers: [
+    {
+      provide: 'DebtConsumer',
+      useFactory: (debtRepository: DebtRepository) => {
+        return new DebtConsumer(debtRepository);
+      },
+      inject: [RepositoryProviders.IDebtRepository],
+    },
+  ],
 })
 export class WorkersModule {}
