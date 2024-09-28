bb301154e4e1b196a23e6a6c47622046c98fbdd4
Eduardo Castellini <eduardo.castellini@kanastra.com.br>
2024-09-28 13:26:07 -0300

    chore: migration

--- prisma/migrations/20240528011505_init/migration.sql
+++ prisma/migrations/20240528011505_init/migration.sql
@@ -4,13 +4,16 @@ CREATE TABLE "Users" (
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Debts" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
     "dueDate" TIMESTAMP(3) NOT NULL,
@@ -18,25 +21,36 @@ CREATE TABLE "Debts" (
     "paymentDate" TIMESTAMP(3),
     "tags" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Debts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Incomes" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
     "dueDate" TIMESTAMP(3) NOT NULL,
     "status" TEXT NOT NULL,
     "tags" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Incomes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Debts_uuid_key" ON "Debts"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Incomes_uuid_key" ON "Incomes"("uuid");
 
 -- AddForeignKey
 ALTER TABLE "Debts" ADD CONSTRAINT "Debts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
